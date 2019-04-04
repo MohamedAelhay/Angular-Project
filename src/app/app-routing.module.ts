3830b68f3d4dcb0a7aea1fa852c0b27f918624e2
MohamedAbdelhay <cap.mohamed.abdelhay@gmail.com>
2019-04-04 19:57:48 +0200

    Creating Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:'', component: HomeComponent},
+  // {path:'login', component: LoginComponent},
+  // {path:'reg', component: RegComponent},
+  // {path:'home', component: GallaryComponent},
+  // {path:'lf', component: LearningFormComponent},
+  // {path:'**', component: ErrCompComponent},
+  // ** -> means if the route doesnt exist will route on ErrCompComponent
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ErrorComponent } from './Components/error/error.component';
+import { InputComponent } from './Components/Share/input/input.component';
+import { ButtonComponent } from './Components/Share/button/button.component';
+import { HeaderComponent } from './Components/Share/header/header.component';
+import { FooterComponent } from './Components/Share/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ErrorComponent,
+    InputComponent,
+    ButtonComponent,
+    HeaderComponent,
+    FooterComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
